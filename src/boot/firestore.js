02d7d6acc61d2fe301e955448a167ae621619944
AtiqurCode <atqur9@gmail.com>
2024-03-04 12:44:59 +0600

    direct env value add in js file

--- src/boot/firestore.js
+++ src/boot/firestore.js
@@ -5,17 +5,26 @@ import { getAuth } from "firebase/auth";
 
 // ... other firebase imports
 // configuration with .env files
+// var firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+// };
+
 var firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyDqv2y-GpipRQMHwKzYwLCN4CHS7csdbo0",
+  authDomain: "fir-test-8b7e6.firebaseapp.com",
+  projectId: "fir-test-8b7e6.appspot.com",
+  storageBucket: "fir-test-8b7e6.appspot.com",
+  messagingSenderId: "666600484295",
+  appId: "1:666600484295:web:2818cf96b5daf2f81cfde2",
 };
 console.log(firebaseConfig);
 console.log("FIREBASE_API_KEY", process.env.FIREBASE_API_KEY);
-console.log("process_env", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("process_env", import.meta.env);
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp); // auth object for the firebase
 
